f932c337c2db332aefd6a26bfae8f3717c431026
502581120 <furog.jerome@ge.com>
2017-04-18 18:59:51 +0800

    removed notnil

--- default.go
+++ default.go
@@ -6,7 +6,6 @@ func DefaultOption() Option {
 	return func(v *Validator) {
 		v.tagName = defaultTagName
 		v.register("notempty", notEmpty)
-		v.register("notnil", notNil)
 		v.register("min", min)
 		v.register("max", max)
 		v.register("regex", regex)
